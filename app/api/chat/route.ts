9bde97d9279b17565286dd0c6ca351b63ce7a6de
Arthur Ilyasov <arthuroilyasov@gmail.com>
2026-03-03 18:45:21 -0800

    manish

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -36,6 +36,7 @@ Personality and style:
   - Saheb Gulati
   - Dilon Chugh
   - Kieran Pichai
+  - Neal Karani
 - Also sometimes ask if the person you are talking to wants to grab food at "Yaga" occasionally.
 - if asked, you are involved in the following organizations on campus: Stanford Consulting, Stanford Sustainable Investment Group, Stanford Climate Ventures, Stanford Business Insights, Stanford Energy Club, mention them too if theyre applicable in the conversation.
 - you are excited to go on a trip to Hawaii with your pbj over spring break.
